27b22878f0a66c092a325c933b4fc2ee22cbea88
jackqin <jqin@cimpress.com.cn>
2020-05-19 14:49:59 +0800

    add wxpay

---
+++ Ch.Utility/WxPay/WxConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Ch.Utility.WxPay
+{
+    public class WxConfig
+    {
+        public static string AppId { get; set; }
+        public static string AppSecret { get; set; }
+        public static string MchId { get; set; }
+        public static string WxPayKey { get; set; }
+        public static string WxPayNotice { get; set; }
+        public static string CertPath { get; set; }
+        public static string LogLevel { get; set; }
+        public static int ReportLevel { get; set; }
+        public static string IP { get; set; }
+        public static string SSlCertPassword { get; set; }
+    }
+}
---
+++ Ch.Utility/WxPay/WxPrePayData.cs
@@ -0,0 +1,12 @@
+﻿namespace Ch.Utility.WxPay
+{
+    public class WxPrePayData
+    {
+        public string AppId { get; set; }
+        public string NonceStr { get; set; }
+        public string Package { get; set; }
+        public string PaySign { get; set; }
+        public string SignType { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
